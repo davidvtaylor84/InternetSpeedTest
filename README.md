51048ce7453c99f1865b953d6f6f440ded91e9ee
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2023-04-28 13:02:50 +0100

    changed parameters

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Internet Speed Test
 
-Allows the user to test speed of local internet connection, measuring Download and Upload speeds, as well as the Ping time. Download speeds will pass if over 100 Mbps and Upload speeds will pass if they are over 20 Mbps. Ping time will pass if it is below 50ms.
+Allows the user to test speed of local internet connection, measuring Download and Upload speeds, as well as the Ping time. Download speeds will pass if over 50 Mbps and Upload speeds will pass if they are over 10 Mbps. Ping time will pass if it is below 50ms.
 
 ## Installation
 
@@ -15,3 +15,5 @@ pip install speedtest-cli
 ```python
 Run in Terminal: python3 internet_speed.py
 ```
+
+
--- internet_speed.py
+++ internet_speed.py
@@ -17,8 +17,8 @@ upload_speed = speed_test.upload()
 ping_outcome = speed_test.results.ping
 
 # pass or fail
-download_speed_pass_or_fail = "Download speed is more than 100 Mbps.\nAdvised Verdict: PASS" if download_speed /1024 /1024 >= 100 else "Download speed is less than 100.\nAdvised Verdict: FAIL"
-upload_speed_pass_or_fail = "Upload speed is more than 20 Mbps.\nAdvised Verdict: PASS" if upload_speed /1024 /1024 >= 20 else "Upload speed is less than 20.\nAdvised Verdict: FAIL"
+download_speed_pass_or_fail = "Download speed is more than 50 Mbps.\nAdvised Verdict: PASS" if download_speed /1024 /1024 >= 50 else "Download speed is less than 50 Mbps.\nAdvised Verdict: FAIL"
+upload_speed_pass_or_fail = "Upload speed is more than 10 Mbps.\nAdvised Verdict: PASS" if upload_speed /1024 /1024 >= 10 else "Upload speed is less than 10 Mbps.\nAdvised Verdict: FAIL"
 ping_pass_or_fail = "Ping time is less than 50ms.\nAdvised Verdict: PASS" if ping_outcome <= 50 else "Ping time is more than 50.\nAdvised Verdict: FAIL"
 
 # print download speed 
